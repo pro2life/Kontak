24d9d244c762c4185694b8cbb701cf3756c83fc0
pro2life <prolife202022@gmail.com>
2022-07-29 14:16:54 +0300

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    history: {
+        key: "slide",
+    },
+});
